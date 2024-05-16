97e94fa25dd328abaa689e6c4cfa63226d491952
Administrator <admin@example.com>
2024-05-16 17:49:18 +0800

    feat: 购物车页面

---
+++ pages/cart/utils/cartService.js
@@ -0,0 +1,44 @@
+import { cleanObject } from "../../../../../utils/index";
+import BaseService from "../../../services/baseService";
+
+class CartService extends BaseService {
+  async getCartList() {
+    return await this.get({
+      url: `${this.baseUrl}/cart/list`,
+      loadingTitle: "加载中...",
+    });
+  }
+
+  async fastAddCart(goodsId, selectedSkuIndex, number) {
+    return await this.post({
+      url: `${this.baseUrl}/cart/fast_add`,
+      data: { goodsId, selectedSkuIndex, number },
+    });
+  }
+
+  async editCart(id, goodsId, selectedSkuIndex, number, success) {
+    return await this.post({
+      url: `${this.baseUrl}/cart/edit`,
+      data: { id, goodsId, selectedSkuIndex, number },
+      success,
+    });
+  }
+
+  async deleteCartList(ids, success) {
+    return await this.post({
+      url: `${this.baseUrl}/cart/delete`,
+      data: { ids },
+      success,
+    });
+  }
+
+  async getGoodsInfo(id) {
+    return await this.get({
+      url: `${this.baseUrl}/goods/detail`,
+      data: { id },
+      loadingTitle: "加载中...",
+    });
+  }
+}
+
+export default CartService;
