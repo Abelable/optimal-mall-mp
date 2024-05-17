c3694c8628e21edabb10b40745469452e2bd64fd
Administrator <admin@example.com>
2024-05-17 10:49:58 +0800

    feat: 我的

---
+++ pages/home/subpages/goods-detail/subpages/evaluation-list/components/evaluation-item/index.js
@@ -0,0 +1,29 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+
+  properties: {
+    contentOmit: Boolean,
+    item: {
+      type: Object,
+      observer({ imageList = [] }) {
+        this.setData({
+          imageList: imageList.slice(0, 3)
+        })
+      }
+    }
+  },
+
+  data: {
+    imageList: []
+  },
+
+  methods: {
+    previewImage(e) {
+      const { current } = e.currentTarget.dataset;
+      const urls = this.properties.item.imageList;
+      wx.previewImage({ current, urls });
+    },
+  },
+});
