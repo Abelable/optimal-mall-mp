afc00827004a644068016df2ee44e48d51dee6cb
Administrator <admin@example.com>
2024-05-28 23:44:41 +0800

    feat: 注册

--- pages/common/register/index.js
+++ pages/common/register/index.js
@@ -1,47 +1,84 @@
-import { WEBVIEW_BASE_URL } from '../../../config'
-import BaseService from '../../../services/baseService'
+import { WEBVIEW_BASE_URL } from "../../../config";
+import BaseService from "../../../services/baseService";
 
-const baseService = new BaseService()
-const { statusBarHeight } = getApp().globalData.systemInfo
+const baseService = new BaseService();
+const { statusBarHeight } = getApp().globalData.systemInfo;
 
 Page({
   data: {
     statusBarHeight,
-    showAuthModal: false
+    agree: false,
+    authModalVisible: false,
+    avatarUrl: "https://img.ubo.vip/youmeng/default_avatar.png"
   },
 
   async getMobile(e) {
-    const mobile = await baseService.getUserMobile(e.detail.code)
+    const mobile = await baseService.getUserMobile(e.detail.code);
     if (mobile) {
-      this.mobile = mobile
-      this.setData({ showAuthModal: true })
+      this.mobile = mobile;
+      this.setData({ authModalVisible: true });
     }
   },
 
-  async getUserInfo() {
-    const { userInfo } = await baseService.getUserProfile()
-    this.register(userInfo)
+  async onChooseAvatar(e) {
+    const avatarUrl = (await baseService.uploadFile(e.detail.avatarUrl)) || "";
+    this.setData({ avatarUrl });
   },
 
-  async register(userInfo) {
-    const { avatarUrl: avatar, nickName: nickname, gender } = userInfo
-    const { code } = await baseService.wxLogin()
-    const token = await baseService.register(code, avatar, nickname, gender, this.mobile)
+  setNickname(e) {
+    this.nickname = e.detail.value;
+  },
+
+  async saveAuthInfo() {
+    if (!this.nickname) {
+      wx.showToast({
+        title: "请输入用户昵称",
+        icon: "none"
+      });
+      return;
+    }
+    await this.register();
+    this.back();
+  },
+
+  async register() {
+    const { code } = await baseService.wxLogin();
+    const token = await baseService.register(
+      code,
+      this.data.avatarUrl,
+      this.nickname,
+      this.mobile
+    );
     if (token) {
-      wx.setStorageSync('token', token)
-      wx.navigateBack()
+      wx.setStorageSync("token", token);
+      wx.navigateBack();
     }
+  },
+
+  toggleAgree() {
+    this.setData({
+      agree: !this.data.agree,
+    });
   },
 
   serviceAgreement() {
-    wx.navigateTo({ url: `/pages/subpages/common/webview/index?url=${WEBVIEW_BASE_URL}/agreements/user_service` })
-  },
-
-  hideModal() {
-    this.setData({ showAuthModal: false })
+    wx.navigateTo({
+      url: `/pages/common/webview/index?url=${WEBVIEW_BASE_URL}/agreements/user_service`
+    });
   },
 
   navToHome() {
-    wx.switchTab({ url: '/pages/tab-bar-pages/home/index' })
+    wx.switchTab({ url: "/pages/tab-bar-pages/home/index" });
+  },
+
+  toast() {
+    wx.showToast({
+      title: "请先阅读并同意有播甄选用户服务协议",
+      icon: "none"
+    });
+  },
+
+  back() {
+    wx.navigateBack();
   }
-})
+});
