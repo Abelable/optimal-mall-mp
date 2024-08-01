1f2cf657ae7bec4b541225ba8b0cbc618dd24fc2
Administrator <admin@example.com>
2024-08-01 16:34:06 +0800

    feat: 诚信乡村

--- pages/rural/index.js
+++ pages/rural/index.js
@@ -4,5 +4,18 @@ const { statusBarHeight } = getApp().globalData.systemInfo;
 Page({
   data: {
     statusBarHeight,
+    navBarBgVisible: false,
+  },
+
+  onPageScroll(e) {
+    if (e.scrollTop >= 10) {
+      if (!this.data.navBarBgVisible) {
+        this.setData({ navBarBgVisible: true });
+      }
+    } else {
+      if (this.data.navBarBgVisible) {
+        this.setData({ navBarBgVisible: false });
+      }
+    }
   },
 });
