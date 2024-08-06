2f2ba479b88256b14dba74956b9fb64cce173203
Administrator <admin@example.com>
2024-08-06 11:35:15 +0800

    feat: 我的账户

--- pages/mine/subpages/account/index.js
+++ pages/mine/subpages/account/index.js
@@ -3,7 +3,15 @@ const { statusBarHeight } = getApp().globalData.systemInfo;
 Page({
   data: {
     statusBarHeight,
-    navBarBgVisible: false
+    navBarBgVisible: false,
+    withdrawAmount: undefined,
+    commission: 0
+  },
+
+  setWithdrawAmount(e) {
+    const withdrawAmount = e.detail.value;
+    const commission = withdrawAmount * 0.001;
+    this.setData({ withdrawAmount, commission });
   },
 
   onPullDownRefresh() {
