115a751b123b78ef32b874c39d895acfd68ab493
Administrator <admin@example.com>
2024-08-06 15:39:57 +0800

    feat: 我的销售

--- pages/mine/subpages/sales-data/index.js
+++ pages/mine/subpages/sales-data/index.js
@@ -1,3 +1,17 @@
 Page({
-  data: {}
+  data: {
+    curMenuIdx: 0,
+    dateList: ["今日", "昨日", "本月", "上月", "全部"],
+    curDateIdx: 0
+  },
+
+  selectMenu(e) {
+    const curMenuIdx = e.currentTarget.dataset.index;
+    this.setData({ curMenuIdx });
+  },
+
+  selectDate(e) {
+    const curDateIdx = e.currentTarget.dataset.index;
+    this.setData({ curDateIdx });
+  },
 });
