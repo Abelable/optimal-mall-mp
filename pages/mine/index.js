2045871c4fd4696ec0d2593d1dd38061a87ba659
Administrator <admin@example.com>
2024-08-10 11:57:49 +0800

    feat: 订单状态已完成改成待评价

--- pages/mine/index.js
+++ pages/mine/index.js
@@ -24,7 +24,7 @@ Component({
       { en: "pay", zh: "待付款", total: 0 },
       { en: "pack", zh: "待发货", total: 0 },
       { en: "delivery", zh: "待收货", total: 0 },
-      { en: "done", zh: "已完成", total: 0 },
+      { en: "done", zh: "待评价", total: 0 },
       { en: "refund", zh: "售后", total: 0 }
     ],
     authInfoModalVisible: false
