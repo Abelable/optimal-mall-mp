d5bf12015c40603511c25a4fa95c84d6185207c9
Administrator <admin@example.com>
2024-08-16 15:29:32 +0800

    feat: 删除团队长

--- pages/mine/subpages/setting/index.js
+++ pages/mine/subpages/setting/index.js
@@ -1,8 +1,2 @@
-import { WEBVIEW_BASE_URL } from "../../../../config";
-
 Page({
-  applyTeamLeader() {
-    const url = `/pages/common/webview/index?url=${WEBVIEW_BASE_URL}/team_leader/settle_in`;
-    wx.navigateTo({ url });
-  }
 });
