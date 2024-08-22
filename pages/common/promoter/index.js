64de047f5571ae3deec6192163e7d995c3985d62
Administrator <admin@example.com>
2024-08-22 16:44:43 +0800

    feat: 我的推广员

--- pages/common/promoter/index.js
+++ pages/common/promoter/index.js
@@ -1,7 +1,18 @@
+import { createStoreBindings } from 'mobx-miniprogram-bindings'
+import { store } from '../../../store/index'
+
 Page({
   data: {
   },
 
   onLoad() {
-  }
+    this.storeBindings = createStoreBindings(this, {
+      store,
+      fields: ['promoterInfo']
+    })
+  },
+
+  onUnload() {
+    this.storeBindings.destroyStoreBindings()
+  },
 })
