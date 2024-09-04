3daaeaffbea3d4f3fd5a243f111a5ea1bef88d04
Administrator <admin@example.com>
2024-09-04 17:30:15 +0800

    feat: 诚信臻品商品列表

--- pages/home/subpages/integrity-goods/components/goods-item/index.js
+++ pages/home/subpages/integrity-goods/components/goods-item/index.js
@@ -7,6 +7,38 @@ Component({
     item: Object
   },
 
+  data: {
+    bottomPrice: 0
+  },
+
+  lifetimes: {
+    attached() {
+      const { price, couponList = [] } = this.properties.item;
+
+      if (couponList.length) {
+        const bottomPrice = couponList.map(
+          ({ type, numLimit, priceLimit, denomination }) => {
+            switch (type) {
+              case 1:
+                return Math.floor((price - denomination) * 100) / 100;
+              case 2:
+                return (
+                  Math.floor(
+                    ((price * numLimit - denomination) / numLimit) * 100
+                  ) / 100
+                );
+              case 3:
+                return priceLimit <= price
+                  ? Math.floor((price - denomination) * 100) / 100
+                  : 0;
+            }
+          }
+        )[0];
+        this.setData({ bottomPrice });
+      }
+    }
+  },
+
   methods: {
     navToGoodsDetail() {
       wx.navigateTo({
