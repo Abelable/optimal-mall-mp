099f1b9aa7f6542d83b061aabba684c1b799a207
Administrator <admin@example.com>
2024-09-04 08:31:29 +0800

    feat: 商品列表

--- components/goods-item/index.js
+++ components/goods-item/index.js
@@ -8,7 +8,36 @@ Component({
   },
 
   data: {
-    visible: false
+    visible: false,
+    bottomPrice: 0
+  },
+
+  lifetimes: {
+    attached() {
+      const { price, couponList = [] } = this.properties.item;
+
+      if (couponList.length) {
+        const bottomPrice = couponList.map(
+          ({ type, numLimit, priceLimit, denomination }) => {
+            switch (type) {
+              case 1:
+                return Math.floor((price - denomination) * 100) / 100;
+              case 2:
+                return (
+                  Math.floor(
+                    ((price * numLimit - denomination) / numLimit) * 100
+                  ) / 100
+                );
+              case 3:
+                return priceLimit <= price
+                  ? Math.floor((price - denomination) * 100) / 100
+                  : 0;
+            }
+          }
+        )[0];
+        this.setData({ bottomPrice });
+      }
+    }
   },
 
   methods: {
