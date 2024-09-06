b35a5ed87a5742082c5654338578330c6f01c0b4
Administrator <admin@example.com>
2024-09-06 15:40:16 +0800

    feat: 商品详情优惠券列表

--- components/coupon-item/index.js
+++ components/coupon-item/index.js
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 Component({
   options: {
     addGlobalClass: true
@@ -7,7 +9,38 @@ Component({
     item: Object,
   },
 
+  data: {
+    countdown: 0
+  },
+
+  lifetimes: {
+    attached() {
+      const { expirationTime } = this.properties.item;
+      const countdown = Math.floor(
+        (dayjs(expirationTime).valueOf() - dayjs().valueOf()) / 1000
+      );
+      this.setData({ countdown });
+      this.setCountdown();
+    },
+
+    detached() {
+      clearInterval(this.countdownInterval);
+    }
+  },
+
   methods: {
+    setCountdown() {
+      this.countdownInterval = setInterval(() => {
+        if (this.data.countdown === 0) {
+          clearInterval(this.countdownInterval);
+          return;
+        }
+        this.setData({
+          countdown: this.data.countdown - 1
+        });
+      }, 1000);
+    },
+    
     navToGoodsDetail() {
       wx.navigateTo({
         url: `/pages/home/subpages/goods-detail/index?id=${this.properties.item.id}`
