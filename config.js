977ab59d2fa04030decec078e0c59bf96c7f7676
Administrator <admin@example.com>
2024-09-10 18:30:35 +0800

    feat: 环境变量

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
-const VERSION = 'v1'
+const VERSION = "v1";
 
 // 环境配置：'pro' - 正式环境, 'dev' - 开发环境
-const ENV = 'pro' 
-const API_BASE_URL = ENV === 'pro' ? 'https://api.youbozhenxuan.cn' : 'http://localhost'
-const WEBVIEW_BASE_URL = ENV === 'pro' ? 'https://h5.youbozhenxuan.cn/#' : 'http://localhost'
-const QQ_MAP_KEY = 'BGCBZ-UFHWX-MBQ4O-TANN2-7WTZ3-CLBIP'
+const ENV = "pro";
+const API_BASE_URL =
+  ENV === "pro"
+    ? "https://api.chengxinxingqiu.com"
+    : "https://api.youbozhenxuan.cn";
+const WEBVIEW_BASE_URL =
+  ENV === "pro"
+    ? "https://h5.chengxinxingqiu.com/#"
+    : "https://h5.youbozhenxuan.cn/#";
 
-export {
-  VERSION,
-  ENV,
-  API_BASE_URL,
-  WEBVIEW_BASE_URL,
-  QQ_MAP_KEY
-}
+export { VERSION, ENV, API_BASE_URL, WEBVIEW_BASE_URL };
