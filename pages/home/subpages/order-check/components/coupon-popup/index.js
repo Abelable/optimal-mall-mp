74da896776bd349277824115a0e646bf3eb84bac
Administrator <admin@example.com>
2024-09-14 15:58:24 +0800

    feat: 订单确认页面优惠券弹窗

---
+++ pages/home/subpages/order-check/components/coupon-popup/index.js
@@ -0,0 +1,69 @@
+import HomeService from "../../utils/homeService";
+
+const homeService = new HomeService();
+
+Component({
+  options: {
+    addGlobalClass: true
+  },
+
+  properties: {
+    addressId: Number,
+    show: {
+      type: Boolean,
+      observer(truthy) {
+        if (truthy) {
+          this.setAddressList();
+        }
+      }
+    }
+  },
+
+  pageLifetimes: {
+    show() {
+      this.setAddressList();
+    }
+  },
+
+  data: {
+    addressList: [],
+    selectedIndex: 0
+  },
+
+  methods: {
+    async setAddressList() {
+      const addressList = await homeService.getAddressList();
+      this.setData({ addressList });
+
+      const { addressId } = this.properties;
+      if (addressId) {
+        const selectedIndex = addressList.findIndex(
+          item => item.id === addressId
+        );
+        this.setData({ selectedIndex });
+      }
+    },
+
+    selectAddress(e) {
+      this.setData({
+        selectedIndex: Number(e.detail.value)
+      });
+    },
+
+    confirm() {
+      const { addressList, selectedIndex } = this.data;
+      const { id } = addressList[selectedIndex]
+      this.triggerEvent("confirm", { id });
+    },
+
+    hide() {
+      this.triggerEvent("hide");
+    },
+
+    navToAddressListPage() {
+      wx.navigateTo({
+        url: "/pages/mine/subpages/address-list/index"
+      });
+    }
+  }
+});
