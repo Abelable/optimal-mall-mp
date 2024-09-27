f9030aa61cc8f477e0f92a304a8a4006e46d24ab
Administrator <admin@example.com>
2024-09-27 18:01:39 +0800

    feat: 售后申请

--- pages/mine/subpages/order-center/subpages/refund/index.js
+++ pages/mine/subpages/order-center/subpages/refund/index.js
@@ -1,8 +1,14 @@
 Page({
   data: {
+    typeList: ["仅退款", "退货退款"],
+    type: undefined,
     applyInfo: {}
   },
 
-  async onLoad({ orderId, goodsId }) {
-  },
+  async onLoad({ orderId, goodsId }) {},
+
+  selectType(e) {
+    const type = Number(e.detail.value);
+    this.setData({ type });
+  }
 });
