e045cdd9e617401df33cd922501890aac4610305
Administrator <admin@example.com>
2024-09-29 14:25:12 +0800

    chore: 正式环境域名.com换.cn

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ const ACTIVITY_TEMPLATE_ID = "Dt7P0wZS10kpAsLFVGSgnG0L2g9D_XNnBfD3w3J9-v0";
 const ENV = "dev";
 const API_BASE_URL =
   ENV === "pro"
-    ? "https://api.chengxinxingqiu.com"
+    ? "https://api.chengxinxingqiu.cn"
     : "https://api.youbozhenxuan.cn";
 const WEBVIEW_BASE_URL =
   ENV === "pro"
-    ? "https://h5.chengxinxingqiu.com/#"
+    ? "https://h5.chengxinxingqiu.cn/#"
     : "https://h5.youbozhenxuan.cn/#";
 
 export { VERSION, ACTIVITY_TEMPLATE_ID, ENV, API_BASE_URL, WEBVIEW_BASE_URL };
