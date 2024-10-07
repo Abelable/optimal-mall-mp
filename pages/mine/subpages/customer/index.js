910066bd0eaef4d2bb3ee1d4477c83f20bcbd692
Administrator <admin@example.com>
2024-10-07 20:18:52 +0800

    feat: 我的客户

--- pages/mine/subpages/customer/index.js
+++ pages/mine/subpages/customer/index.js
@@ -1,10 +1,30 @@
 Page({
   data: {
+    curMenuIdx: 0,
+    keywords: "",
+    searching: false
   },
 
-  async onLoad(options) {
+  onLoad(options) {
+    const type = +options.type;
+    if (type !== 0) {
+      this.setData({ curMenuIdx: type - 1 });
+    }
   },
 
-  onShow() {
+  selectMenu(e) {},
+
+  setKeywords(e) {
+    this.setData({
+      keywords: e.detail.value
+    });
   },
+
+  search() {
+    this.setData({ searching: true });
+  },
+
+  clearSearch() {
+    this.setData({ searching: false });
+  }
 });
