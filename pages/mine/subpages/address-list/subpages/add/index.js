773d56631e6bd9f9c11673d24e0a957db7abc9a9
Administrator <admin@example.com>
2024-11-22 23:22:52 +0800

    feat: 地址智能分析

--- pages/mine/subpages/address-list/subpages/add/index.js
+++ pages/mine/subpages/address-list/subpages/add/index.js
@@ -1,75 +1,116 @@
-import AddressService from '../../utils/addressService'
+import AddressService from "../../utils/addressService";
 
-const addressService = new AddressService()
+const addressService = new AddressService();
 
 Page({
   data: {
-    regionDesc: ''
+    text: "",
+    regionDesc: "",
+    name: "",
+    addressDetail: ""
+  },
+
+  textInput(e) {
+    this.setData({ text: e.detail.value });
+  },
+
+  clearText() {
+    this.setData({ text: "" });
+  },
+
+  async recognizeText() {
+    if (!this.data.text) {
+      wx.showToast({
+        title: "请输入识别地址",
+        icon: "none"
+      });
+      return;
+    }
+    const {
+      address,
+      province,
+      province_code,
+      city,
+      city_code,
+      county,
+      county_code,
+      person: name,
+      phonenum: mobile
+    } = await addressService.analyzeAddress(this.data.text);
+
+    const regionDesc = `${province}${city}${county}`;
+    this.regionCodeList = [province_code, city_code, county_code];
+    const addressDetail = address
+      .replace(province, "")
+      .replace(city, "")
+      .replace(county, "");
+    this.setData({ name, mobile, regionDesc, addressDetail });
   },
 
   setName(e) {
-    this.name = e.detail.value
+    this.setData({ name: e.detail.value });
   },
 
   setMobile(e) {
-    this.mobile = e.detail.value
+    this.setData({ mobile: e.detail.value });
   },
 
   selectRegion(e) {
-    const { code, value } = e.detail
-    this.regionCodeList = code
-    const regionDesc = Array.from(new Set(value)).join(' ')
-    this.setData({ regionDesc })
+    const { code, value } = e.detail;
+    this.regionCodeList = code;
+    const regionDesc = Array.from(new Set(value)).join(" ");
+    this.setData({ regionDesc });
   },
 
   setAddressDetail(e) {
-    this.addressDetail = e.detail.value
+    this.setData({ addressDetail: e.detail.value });
   },
 
   toggleDefaultValue(e) {
-    this.isDefault = e.detail.value ? 1 : 0
+    this.isDefault = e.detail.value ? 1 : 0;
   },
 
   save() {
-    if (!this.name) {
-      wx.showToast({ 
-        title: '请输入姓名', 
-        icon: 'none' 
-      })
-      return
+    const { name, mobile, regionDesc, addressDetail } = this.data;
+    if (!name) {
+      wx.showToast({
+        title: "请输入姓名",
+        icon: "none"
+      });
+      return;
     }
-    if (!this.mobile || !/^1[345789][0-9]{9}$/.test(this.mobile)) {
-      wx.showToast({ 
-        title: '请输入正确手机号', 
-        icon: 'none' 
-      })
-      return
+    if (!mobile || !/^1[345789][0-9]{9}$/.test(mobile)) {
+      wx.showToast({
+        title: "请输入正确手机号",
+        icon: "none"
+      });
+      return;
     }
     if (!this.regionCodeList) {
-      wx.showToast({ 
-        title: '请选择省市区', 
-        icon: 'none' 
-      })
-      return
+      wx.showToast({
+        title: "请选择省市区",
+        icon: "none"
+      });
+      return;
     }
-    if (!this.addressDetail) {
-      wx.showToast({ 
-        title: '请输入详细地址', 
-        icon: 'none' 
-      })
-      return
+    if (!addressDetail) {
+      wx.showToast({
+        title: "请输入详细地址",
+        icon: "none"
+      });
+      return;
     }
 
     addressService.addAddress(
-      this.name, 
-      this.mobile, 
-      JSON.stringify(this.regionCodeList), 
-      this.data.regionDesc, 
-      this.addressDetail, 
+      name,
+      mobile,
+      JSON.stringify(this.regionCodeList),
+      regionDesc,
+      addressDetail,
       this.isDefault,
       () => {
-        wx.navigateBack()
+        wx.navigateBack();
       }
-    )
+    );
   }
-})
+});
