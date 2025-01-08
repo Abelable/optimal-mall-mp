1096c8f677025f23d6338b705b279c42b3605315
funguy <1209683835@qq.com>
2025-01-08 11:24:58 +0800

    feat: 鉴权处理

--- pages/common/register/index.js
+++ pages/common/register/index.js
@@ -65,11 +65,14 @@ Page({
     const pagesLength = getCurrentPages().length;
     const prePage = getCurrentPages()[pagesLength - 2];
     const prePageRoute = prePage ? prePage.route : "";
+    console.log("prePageRoute", prePageRoute);
     if (
       prePageRoute === "pages/mine/index" ||
       prePageRoute === "pages/cart/index"
     ) {
       wx.switchTab({ url: "/pages/home/index" });
+    } else if (prePageRoute === "pages/home/subpages/cart/index") {
+      wx.navigateBack({ delta: 2 });
     } else {
       wx.navigateBack();
     }
