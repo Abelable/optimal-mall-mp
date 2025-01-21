57919902b1feded92eb86fd7dee742d14458bcf1
funguy <1209683835@qq.com>
2025-01-21 11:59:51 +0800

    feat: 诚信臻品分享

--- pages/home/subpages/integrity-goods/index.js
+++ pages/home/subpages/integrity-goods/index.js
@@ -1,3 +1,4 @@
+import { store } from "../../../../store/index";
 import IntegrityService from "./utils/integrityService";
 
 const integrityService = new IntegrityService();
@@ -19,13 +20,22 @@ Page({
   },
 
   async setGoodsList() {
-    const goodsList =
-      (await integrityService.getGoodsList()) || [];
+    const goodsList = (await integrityService.getGoodsList()) || [];
     this.setData({ goodsList });
   },
 
   onPullDownRefresh() {
     this.setGoodsList();
     wx.stopPullDownRefresh();
+  },
+
+  onShareAppMessage() {
+    const { id } = store.promoterInfo || {};
+    const originalPath = `/pages/home/subpages/integrity-goods/index?url=${this.webviewUrl.replace(
+      "?",
+      "&"
+    )}`;
+    const path = id ? `${originalPath}&superiorId=${id}` : originalPath;
+    return { path };
   }
 });
