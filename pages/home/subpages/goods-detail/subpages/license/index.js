00611885eae0b4ac882bba69c82c6fa9a6e0fd99
funguy <1209683835@qq.com>
2025-03-18 15:30:33 +0800

    feat: 企业资质图片缩放

--- pages/home/subpages/goods-detail/subpages/license/index.js
+++ pages/home/subpages/goods-detail/subpages/license/index.js
@@ -6,7 +6,7 @@ Page({
   async onLoad({ license }) {
     const licenseList = JSON.parse(license).map(
       item =>
-        `${item}?x-oss-process=image/resize,w_600/watermark,type_d3F5LXplbmhlaQ,size_12,text_5LuF5L6b6K-a5L-h5pif55CD5bmz5Y-w5bGV56S65L2_55SoLCDku5bnlKjml6DmlYg=,color_999999,rotate_315,t_100,g_se,x_10,y_10,fill_1`
+        `${item}?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,size_24,text_5LuF5L6b6K-a5L-h5pif55CD5bmz5Y-w5bGV56S65L2_55SoLCDku5bnlKjml6DmlYg=,color_999999,rotate_315,t_100,g_se,x_10,y_10,fill_1`
     );
     this.setData({ licenseList });
   },
