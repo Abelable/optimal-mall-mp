955656354caa83b2239908ed4d6007867f028d04
funguy <1209683835@qq.com>
2025-08-11 20:24:13 +0800

    feat: 直播

--- pages/subpages/live/create-live/components/goods-picker-popup/index.js
+++ pages/subpages/live/create-live/components/goods-picker-popup/index.js
@@ -30,42 +30,44 @@ Component({
 
     async setGoodsList(init = false) {
       if (init) this.page = 0;
-      const { list = [] } =
-        (await liveService.getUserGoodsList(++this.page)) || {};
-      const goodsList = list.map((item) => ({ ...item, checked: false }));
+      const list = (await liveService.getGoodsList(++this.page)) || {};
+      const goodsList = list.map(item => ({
+        ...item,
+        checked: false
+      }));
       this.setData({
-        goodsList: init ? goodsList : [...this.data.goodsList, ...goodsList],
+        goodsList: init ? goodsList : [...this.data.goodsList, ...goodsList]
       });
     },
 
     toggleChecked(e) {
       const { index } = e.currentTarget.dataset;
       this.setData({
-        [`goodsList[${index}].checked`]: !this.data.goodsList[+index].checked,
+        [`goodsList[${index}].checked`]: !this.data.goodsList[+index].checked
       });
       this.setData({
         allChecked: this.data.goodsList.findIndex(item => !item.checked) === -1
-      })
+      });
     },
 
     toggleAllChecked() {
-      const { goodsList: list, allChecked } = this.data
-      const goodsList = list.map(item => ({ ...item, checked: !allChecked }))
+      const { goodsList: list, allChecked } = this.data;
+      const goodsList = list.map(item => ({ ...item, checked: !allChecked }));
       this.setData({
         goodsList,
         allChecked: !allChecked
-      })
+      });
     },
 
     confirm() {
       const goodsIds = this.data.goodsList
-        .filter((item) => item.checked)
-        .map((item) => item.id);
+        .filter(item => item.checked)
+        .map(item => item.id);
       this.triggerEvent("confirm", goodsIds);
     },
 
     hide() {
       this.triggerEvent("hide");
-    },
-  },
+    }
+  }
 });
