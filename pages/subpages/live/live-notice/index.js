967542d2fe57c05eeb1f32a0204490c72256a02f
funguy <1209683835@qq.com>
2025-08-12 12:49:55 +0800

    feat: 直播

--- pages/subpages/live/live-notice/index.js
+++ pages/subpages/live/live-notice/index.js
@@ -1,65 +1,63 @@
-import LiveService from '../utils/liveService'
+import LiveService from "../utils/liveService";
 
-const liveService = new LiveService()
-const { statusBarHeight } = getApp().globalData.systemInfo
+const liveService = new LiveService();
+const { statusBarHeight } = getApp().globalData.systemInfo;
 
 Page({
   data: {
     statusBarHeight,
     roomInfo: null,
-    countDown: 0,
+    countDown: 0
   },
 
   onLoad() {
-    this.setRoomInfo()
+    this.setRoomInfo();
   },
 
   async setRoomInfo() {
-    const roomInfo = await liveService.getNoticeRoomInfo()
+    const roomInfo = await liveService.getNoticeRoomInfo();
     if (roomInfo) {
-      this.setData({ roomInfo })
-      this.setCountDown(new Date(roomInfo.noticeTime).getTime())
+      this.setData({ roomInfo });
+      this.setCountDown(new Date(roomInfo.noticeTime).getTime());
     }
   },
 
   setCountDown(startTime) {
-    const currentTime = (new Date()).getTime()
-    let countDown = (startTime - currentTime) / 1000
-    this.setData({ countDown })
+    const currentTime = new Date().getTime();
+    let countDown = (startTime - currentTime) / 1000;
+    this.setData({ countDown });
     this.countDownInterval = setInterval(() => {
       if (countDown > 0) {
-        --countDown
-        this.setData({ countDown })
-      } else clearInterval(this.countDownInterval)
-    }, 1000)
+        --countDown;
+        this.setData({ countDown });
+      } else clearInterval(this.countDownInterval);
+    }, 1000);
   },
 
   deleteLiveNotice() {
     wx.showModal({
-      content: '确定删除直播预告吗？',
-      success: (result) => {
-        if(result.confirm){
+      content: "确定删除直播预告吗？",
+      success: result => {
+        if (result.confirm) {
           liveService.deleteNoticeRoom(() => {
             wx.showToast({
-              title: '删除成功',
-              icon: 'none',
-            })
+              title: "删除成功",
+              icon: "none"
+            });
             setTimeout(() => {
-              this.navBack()
-            }, 2000)
-          })
+              this.navBack();
+            }, 2000);
+          });
         }
       }
-    })
+    });
   },
 
   navBack() {
-    wx.switchTab({
-      url: '/pages/mine/index'
-    })
+    wx.navigateBack();
   },
 
   onUnload() {
-    clearInterval(this.countDownInterval)
+    clearInterval(this.countDownInterval);
   }
-})
+});
