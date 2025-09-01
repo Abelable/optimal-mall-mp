620d136c26d5769e5074370889f1c7b2f83b2237
funguy <1209683835@qq.com>
2025-09-01 18:30:16 +0800

    feat: 直播列表

--- pages/subpages/live/live-list/index.js
+++ pages/subpages/live/live-list/index.js
@@ -1,9 +1,12 @@
 import LiveService from "../utils/liveService";
 
 const liveService = new LiveService();
+const { statusBarHeight } = getApp().globalData.systemInfo;
 
 Page({
   data: {
+    statusBarHeight,
+    navBarBgVisible: false,
     liveList: [],
     finished: false
   },
@@ -21,11 +24,23 @@ Page({
     wx.stopPullDownRefresh();
   },
 
+  onPageScroll(e) {
+    if (e.scrollTop >= 10) {
+      if (!this.data.navBarBgVisible) {
+        this.setData({ navBarBgVisible: true });
+      }
+    } else {
+      if (this.data.navBarBgVisible) {
+        this.setData({ navBarBgVisible: false });
+      }
+    }
+  },
+
   async setLiveList(init = false) {
     if (init) {
       this.page = 0;
     }
-    
+
     const { list = [] } =
       (await liveService.getLiveList({ page: ++this.page })) || {};
     this.setData({ liveList: init ? list : [...this.data.liveList, ...list] });
